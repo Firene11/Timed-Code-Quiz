77b7c6362cd47bdb3fb927dc9d51cf1a20831904
Fih <124883284+Firene11@users.noreply.github.com>
2023-05-29 18:30:17 -0300

    tring to get scores to show up

--- highscores.js
+++ highscores.js
@@ -1,10 +1,5 @@
 var scoreBox = document.getElementById("#scoreBox");
 var localScores = JSON.parse(localStorage.getItem('scores')) || [];
 
-
-document.querySelector("#scorebox").innerHTML = localScores
-    .map(score => {
-        return '<li class="high-scores">${score.initials}</li>';
-    })
-    .join("");
-
+console.log(localScores)
+document.querySelector("#scorebox").textContent = localScores.initials + localScores.score;
